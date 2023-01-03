304eaf0d1f6c4c677cebbc3405bc6c14752a788a
Elkin Duran <44894517+elkindm@users.noreply.github.com>
2023-01-03 12:31:53 -0500

    inicial

---
+++ src/test/java/co/com/libisoft/curso/CursoApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.libisoft.curso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
