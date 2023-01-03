e49f00513ccee6862a57c20409cbb3e9693da64d
Elkin Duran <44894517+elkindm@users.noreply.github.com>
2023-01-03 13:10:13 -0500

    clase 12 y 13, se agrega la arqutectura en base al dominio y se agrega el repositorio de spring data jpa

--- src/main/java/co/com/libisoft/curso/web/controller/HolaMundoController.java
+++ src/main/java/co/com/libisoft/curso/web/controller/HolaMundoController.java
@@ -1,4 +1,4 @@
-package co.com.libisoft.curso.controller;
+package co.com.libisoft.curso.web.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
