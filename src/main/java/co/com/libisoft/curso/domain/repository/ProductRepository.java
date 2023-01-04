ec381aec08aca0fdc1dbd806025e32c2e528bee5
Elkin Duran <44894517+elkindm@users.noreply.github.com>
2023-01-04 12:28:24 -0500

    clase 23, repositorio orientado al dominio

--- src/main/java/co/com/libisoft/curso/domain/repository/ProductRepository.java
+++ src/main/java/co/com/libisoft/curso/domain/repository/ProductRepository.java
@@ -8,9 +8,9 @@ import java.util.Optional;
 
 public interface ProductRepository {
     List<Product> getAll();
-    Optional<List<Producto>> getByCategory(int categoryId);
-    Optional<List<Producto>> getScarseProducts(int quantity);
-    Optional<Producto> getProduct(int productId);
+    Optional<List<Product>> getByCategory(int categoryId);
+    Optional<List<Product>> getScarseProducts(int quantity);
+    Optional<Product> getProduct(int productId);
     Product save(Product product);
     void delete(int productId);
 }
--- src/main/java/co/com/libisoft/curso/percistence/ProductoRepository.java
+++ src/main/java/co/com/libisoft/curso/percistence/ProductoRepository.java
@@ -1,42 +1,51 @@
 package co.com.libisoft.curso.percistence;
 
+import co.com.libisoft.curso.domain.Product;
+import co.com.libisoft.curso.domain.repository.ProductRepository;
 import co.com.libisoft.curso.percistence.crud.ProdutoCrudRepository;
 import co.com.libisoft.curso.percistence.entity.Producto;
+import co.com.libisoft.curso.percistence.mapper.ProductMapper;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
 @Repository
-public class ProductoRepository {
+public class ProductoRepository implements ProductRepository {
 
     private ProdutoCrudRepository porProdutoCrudRepository;
+    private ProductMapper mapper;
 
-    public List<Producto> getAll(){
-        return (List<Producto>) porProdutoCrudRepository.findAll();
+    @Override
+    public List<Product> getAll(){
+        List<Producto> productos = (List<Producto>) porProdutoCrudRepository.findAll();
+        return mapper.toProducts(productos);
     }
 
-    /**
-     *
-     * @param idCategoria
-     * @return List<Producto>
-     */
-    public List<Producto> getByCAtegoria(int idCategoria){
-        return  porProdutoCrudRepository.findByIdCategoriaOrderByNombreAsc(idCategoria);
+    @Override
+    public Optional<List<Product>> getByCategory(int categoryId) {
+        List<Producto> productos = porProdutoCrudRepository.findByIdCategoriaOrderByNombreAsc(categoryId);
+        return Optional.of(mapper.toProducts(productos));
     }
 
-    public Optional<List<Producto>> getEscasos(int cantidad){
-        return porProdutoCrudRepository.findByCantidadStockLessThanAndEstado(cantidad,true);
+    @Override
+    public Optional<List<Product>> getScarseProducts(int quantity) {
+        Optional<List<Producto>> productos = porProdutoCrudRepository.findByCantidadStockLessThanAndEstado(quantity, true);
+        return productos.map(p->mapper.toProducts(p));
     }
 
-    public Optional<Producto> getProducto(int idProducto){
-        return porProdutoCrudRepository.findById(idProducto);
+    @Override
+    public Optional<Product> getProduct(int productId) {
+        return porProdutoCrudRepository.findById(productId).map(p->mapper.toProduct(p));
     }
 
-    public Producto save(Producto producto){
-        return porProdutoCrudRepository.save(producto);
+    @Override
+    public Product save(Product product) {
+        Producto producto = mapper.toProducto(product);
+        return mapper.toProduct(porProdutoCrudRepository.save(producto));
     }
 
+    @Override
     public void delete(int idProducto){
          porProdutoCrudRepository.deleteById(idProducto);
     }
